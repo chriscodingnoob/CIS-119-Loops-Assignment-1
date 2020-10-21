e2037466fe19821e330aee3dee73fc60a7c116ca
Arthur <Arthur@Arthur-Laptop.niacc.edu>
2020-10-21 10:12:12 -0500

    Commit 2

--- public_html/LoopsAssignment1.js
+++ public_html/LoopsAssignment1.js
@@ -5,3 +5,9 @@
 
 document.write("This program displays the numbers 0 to 1000 by 10's");
 
+let counter = 10;
+while (counter <= 1000) {
+    document.write(counter + "<br>");
+    counter = counter + 10;
+}
+document.write("Goodbye");
